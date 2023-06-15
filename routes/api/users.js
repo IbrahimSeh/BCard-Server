6956740d6e0231e5ef55afbf994613b76ad38a31
Ibrahim seh <ibrahim-seh@outlook.com>
2023-06-15 14:25:10 +0300

    register & login + npm i jsonweb token joi encyptjs

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,62 @@
 const express = require("express");
 const router = express.Router();
+const hashService = require("../../utils/hash/hashService");
+const {
+    registerUserValidation,
+    loginUserValidation,
+} = require("../../validation/userValidationService");
+const normalizeUser = require("../../model/usersService/helpers/normalizationUserService");
+const userQueriesModel = require("../../model/usersService/usersQueries");
+const { generateToken } = require("../../utils/token/tokenService");
+const CustomError = require("../../utils/CustomError");
 
-router.get("/", async (req, res) => {
+//http://localhost:8181/api/users
+router.post("/", async (req, res) => {
     try {
-        res.json({ "msg": "http://localhost:8182/api/users/" });
+        /*
+         * joi
+         * email unique - mongoose -> mongo
+         * encrypt the password
+         * normalize
+         * create user
+         * response user created
+         */
+        await registerUserValidation(req.body);
+        req.body.password = await hashService.generateHash(req.body.password);
+        req.body = normalizeUser(req.body);
+        await userQueriesModel.registerUser(req.body);
+        res.json({ msg: "register" });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+//http://localhost:8181/api/users/login
+router.post("/login", async (req, res) => {
+    try {
+        /**
+         * *joi
+         * *get user from database
+         * *check password
+         * *create token
+         * *send to user  
+         */
+        await loginUserValidation(req.body);
+        const userData = await userQueriesModel.getUserByEmail(req.body.email);
+        if (!userData) throw new CustomError("invalid email and/or password");
+        const isPasswordMatch = await hashService.cmpHash(
+            req.body.password,
+            userData.password
+        );
+        if (!isPasswordMatch)
+            throw new CustomError("invalid email and/or password");
+        const token = await generateToken({
+            _id: userData._id,
+            isAdmin: userData.isAdmin,
+            isBusiness: userData.isBusiness,
+        });
+        res.json({ token });
     } catch (err) {
         res.status(400).json(err);
     }
